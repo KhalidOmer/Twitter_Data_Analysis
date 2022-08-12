a8986033fad5c79835ca12ffb30e072e7e0bacc7
Khalid <khalido.amin@gmail.com>
2022-08-13 00:16:29 +0200

    editing the extraction module

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -106,7 +106,7 @@ class TweetDfExtractor:
         """required column to be generated you should be creative and add more features"""
         
         columns = ['created_at', 'source', 'original_text','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
-            'original_author', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'statuses_count']
+            'original_author', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'statuses_count', 'screen_name']
         
         created_at = self.find_created_time()
         source = self.find_source()
@@ -123,7 +123,7 @@ class TweetDfExtractor:
         mentions = self.find_mentions()
         location = self.find_location()
         statuses_count = self.find_statuses_count()
-        data = zip(created_at, source, text, polarity, subjectivity, lang, fav_count, retweet_count, screen_name, follower_count, friends_count, sensitivity, hashtags, mentions, location, statuses_count)
+        data = zip(created_at, source, text, polarity, subjectivity, lang, fav_count, retweet_count, screen_name, follower_count, friends_count, sensitivity, hashtags, mentions, location, statuses_count, screen_name)
         df = pd.DataFrame(data=data, columns=columns)
 
         if save:
